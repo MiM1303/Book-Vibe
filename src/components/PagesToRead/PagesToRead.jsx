62f294006f94992ca6d4d387c6899f97a4f52d3a
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-03-26 06:18:11 +0600

    added pages to read component functionality

--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,9 +1,20 @@
-
-
+import { useEffect, useState } from "react";
+import Chart from "../Chart/Chart";
+import { useLoaderData } from "react-router-dom";
+import { getStoredReadBooks } from "../../utility/localstorage";
 const PagesToRead = () => {
+    const books = useLoaderData();
+    const [readBooks, setReadBooks] = useState([]);
+    useEffect(()=>{
+        const storedBookId = getStoredReadBooks();
+        if(books.length>0){
+            const booksRead = books.filter(book=>storedBookId.includes(book.bookId));
+            setReadBooks(booksRead);
+        }
+    },[])
     return (
         <div>
-            
+            <Chart readBooks={readBooks}></Chart>
         </div>
     );
 };
